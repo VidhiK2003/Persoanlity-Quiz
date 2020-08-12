d7af00d053fef5c47d1bac664036c70a80f9a18c
Glitch (personality-quiz-starter-code) <none>
2020-08-12 17:11:10 +0000

    😀🚝 Checkpoint ./index.html:19946121/29 ./script.js:19946121/556

--- script.js
+++ script.js
@@ -16,6 +16,21 @@ var q1a2 = document.getElementById("q1a2");
 var q1a3 = document.getElementById("q1a3");
 var q1a4 = document.getElementById("q1a4");
 
+var q2a1 = document.getElementById("q1a1");
+var q2a2 = document.getElementById("q1a2");
+var q2a3 = document.getElementById("q1a3");
+var q2a4 = document.getElementById("q1a4");
+
+var q3a1 = document.getElementById("q1a1");
+var q3a2 = document.getElementById("q1a2");
+var q3a3 = document.getElementById("q1a3");
+var q3a4 = document.getElementById("q1a4");
+
+var q4a1 = document.getElementById("q1a1");
+var q4a2 = document.getElementById("q1a2");
+var q4a3 = document.getElementById("q1a3");
+var q4a4 = document.getElementById("q1a4");
+
 
 
 //#TODO: Add Event Listeners to your answer choice variables.
